89a8fa608de2b2ff566bfa8bc96b2ac05c3bbfa7
1415230492 <1415230492@qq.com>
2023-06-28 15:49:37 +0800

    日志在文件和控制台中都输出

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 use log::{error, info, warn, debug, LevelFilter};
-use log4rs::{append::file::FileAppender, encode::pattern::PatternEncoder, Config, config::{Appender, Root}};
+use log4rs::{append::{file::FileAppender, console::ConsoleAppender}, encode::pattern::PatternEncoder, Config, config::{Appender, Root}};
 use test1::conf::cli::load_conf;
 
 
@@ -9,19 +9,16 @@ async fn main() {
     let c = load_conf();
     println!("{}", c.get_log_level());
 
+    let stdout = ConsoleAppender::builder().build();
     let logfile = FileAppender::builder()
         .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
         .build("log/output.log").unwrap();
 
     let config = Config::builder()
+        .appender(Appender::builder().build("console", Box::new(stdout)))
         .appender(Appender::builder().build("logfile", Box::new(logfile)))
-        .build(Root::builder()
-        .appender("logfile")
-        .build(LevelFilter::Warn))
-        .appender("stderr") //
+        .build(Root::builder().appender("console").appender("logfile").build(LevelFilter::Debug))
         .unwrap();
-    let t = config.appender(Appender::builder().filter(Box::new(ThresholdFilter::new(level))).build("stderr", Box::new(stderr)))
-            .build(Root::builder().appender("logfile").appender("stderr").build(level)).unwrap();
 
     log4rs::init_config(config).unwrap();
 
